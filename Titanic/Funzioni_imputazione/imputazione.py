b2297f99c7f08751a397d06ee9aabe184170765a
davidevitale <dvitale2002@gmail.com>
2025-07-02 13:10:20 +0200

    Update imputazione.py

--- Titanic/Funzioni_imputazione/imputazione.py
+++ Titanic/Funzioni_imputazione/imputazione.py
@@ -2,4 +2,6 @@ from Funzioni_imputazione.riempi_Home_Planet import riempi_Home_Planet
 
 def imputazione(df):
     df = riempi_Home_Planet(df)
+    df = riempi_Destination(df)
+    df = riempi_Surname(df)
     return df
