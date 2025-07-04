850e2b39a6af70a6ec0c6b3085dd729849b484d9
Valerio-Minorenti <v.minorenti.02@gmail.com>
2025-07-04 18:11:37 +0200

    me

--- Titanic/Funzioni_imputazione/imputazione.py
+++ Titanic/Funzioni_imputazione/imputazione.py
@@ -5,6 +5,6 @@ from Funzioni_imputazione.riempi_Destination import riempi_Destination
 
 def imputazione(df):
     df = riempi_Home_Planet(df)
-    df = riempi_Surname(df)
     df= riempi_Destination(df)
+    df = riempi_Surname(df)
     return df
