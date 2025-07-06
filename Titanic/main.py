7211dfc158cbc0742d28792ff45f3e6fb29e3a7e
davidevitale <dvitale2002@gmail.com>
2025-07-06 12:31:15 +0200

    Add files via upload

--- Titanic/main.py
+++ Titanic/main.py
@@ -1,16 +1,28 @@
 from Funzioni_imputazione.converti_valori_e_colonne import converti_valori_colonne
 from Funzioni_imputazione.imputazione import imputazione
+from hold import holdout_split
 
+def main():
+    # Percorso file input e output
+    excel_file = 'C:/Users/dvita/Desktop/TITANIC/train.xlsx'
+    output_train = 'C:/Users/dvita/Desktop/TITANIC/train_holdout.xlsx'
+    output_val = 'C:/Users/dvita/Desktop/TITANIC/val_holdout.xlsx'
 
-csv_file = 'C:/Users/Standard/Desktop/Titanic/Titanic/train.csv'
-output_file = 'C:/Users/Standard/Desktop/Titanic/Titanic/Funzioni_imputazione/tabellozza.xlsx'
-excel_file = 'C:/Users/Standard/Desktop/Titanic/Titanic/Funzioni_imputazione/tabellozza.xlsx'
-# Converte e prepara
-df = converti_valori_colonne(csv_file, output_file,excel_file)
+    # 1) Split train/val da file iniziale
+    holdout_split(
+        input_path=excel_file, train_path=output_train, val_path=output_val, test_size=0.2, stratify_col='Transported', random_state=42
+    )
 
-# Applica imputazioni
-df = imputazione(df)
+    # 2) Pulizia e conversione colonne, ritorna combined_df
+    combined_df = converti_valori_colonne()
 
-# Salva il file finale aggiornato
-df.to_excel(output_file, index=False)
-print(f"Dataset finale salvato in: '{output_file}'")
+    # 3) Imputazione sul combined_df
+    combined_df = imputazione(combined_df)
+
+    # Puoi salvare df_finale oppure stampare info
+    print("Imputazione completata.")
+
+if __name__ == "__main__":
+    main()
+    print("Esecuzione completata con successo.")
+
